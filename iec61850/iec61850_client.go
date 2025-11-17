e8de5618c9ba39b829b23ba2a88abec74b640072
guoxi.wang <guoxi.wang@weiheng-tech.com>
2025-11-17 14:41:58 +0800

    perf: optimize value resolution and memory allocation in iec61850 client

--- iec61850/iec61850_client.go
+++ iec61850/iec61850_client.go
@@ -1,10 +1,12 @@
 package iec61850
 
 // #include <iec61850_client.h>
+// #include <string.h>
 import "C"
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -210,6 +212,104 @@ func (client *IedClient) resolveValue(value *C.MmsValue, valueType MMSType) inte
 	return goValue
 }
 
+// resolveValueInline 内联版本的值解析，减少interface{}装箱和函数调用开销
+// 使用unsafe优化字符串转换，避免不必要的内存拷贝
+func (client *IedClient) resolveValueInline(target *GoMmsValue, value *C.MmsValue, valueType MMSType) {
+	// Refer to https://support.mz-automation.de/doc/libiec61850/c/latest/group__MMS__VALUE.html
+
+	switch valueType {
+	case MMS_BOOLEAN:
+		target.Value = bool(C.MmsValue_getBoolean(value))
+	case MMS_FLOAT:
+		target.Value = float64(C.MmsValue_toDouble(value))
+	case MMS_INTEGER:
+		target.Value = int64(C.MmsValue_toInt64(value))
+	case MMS_UNSIGNED:
+		target.Value = int64(C.MmsValue_toInt64(value))
+	case MMS_STRING:
+		// 使用unsafe优化：避免C.GoString的内存拷贝
+		cStr := C.MmsValue_toString(value)
+		if cStr != nil {
+			target.Value = cStringToGoString(cStr)
+		} else {
+			target.Value = ""
+		}
+	case MMS_VISIBLE_STRING:
+		cStr := C.MmsValue_toString(value)
+		if cStr != nil {
+			target.Value = cStringToGoString(cStr)
+		} else {
+			target.Value = ""
+		}
+	case MMS_STRUCTURE:
+		target.Value = client.digIntoStructureOptimized(value, valueType)
+	case MMS_ARRAY:
+		target.Value = client.digIntoStructureOptimized(value, valueType)
+	case MMS_BIT_STRING:
+		if client.withoutTimestamps {
+			target.Value = 0
+			return
+		}
+		target.Value = uint32(C.MmsValue_getBitStringAsInteger(value))
+	case MMS_UTC_TIME:
+		if client.withoutTimestamps {
+			target.Value = 0
+			return
+		}
+		target.Value = uint32(C.MmsValue_toUnixTimestamp(value))
+	default:
+		target.Value = nil
+	}
+}
+
+// cStringToGoString 使用unsafe将C字符串高效转换为Go字符串
+// 注意：由于C字符串的生命周期由MmsValue管理，在ClientDataSet销毁时会失效
+// 因此这里仍需要拷贝数据，但优化了拷贝过程
+func cStringToGoString(cStr *C.char) string {
+	if cStr == nil {
+		return ""
+	}
+
+	// 快速获取字符串长度
+	length := int(C.strlen(cStr))
+	if length == 0 {
+		return ""
+	}
+
+	// 使用unsafe直接访问C内存，然后一次性拷贝到Go字符串
+	// 这比C.GoString更高效，因为避免了多次函数调用
+	var b []byte
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	hdr.Data = uintptr(unsafe.Pointer(cStr))
+	hdr.Len = length
+	hdr.Cap = length
+
+	// 必须拷贝数据，因为C内存会被释放
+	return string(b)
+}
+
+// cStringToGoStringUnsafe 真正的零拷贝版本（危险！）
+// 仅在确保C字符串生命周期足够长时使用
+// 返回的字符串直接引用C内存，不进行拷贝
+func cStringToGoStringUnsafe(cStr *C.char) string {
+	if cStr == nil {
+		return ""
+	}
+
+	length := int(C.strlen(cStr))
+	if length == 0 {
+		return ""
+	}
+
+	// 直接构造字符串头，指向C内存（零拷贝）
+	var s string
+	hdr := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	hdr.Data = uintptr(unsafe.Pointer(cStr))
+	hdr.Len = length
+
+	return s
+}
+
 func (client *IedClient) digIntoStructure(mms *C.MmsValue, vType MMSType) []GoMmsValue {
 	if vType != MMS_STRUCTURE {
 		return nil
@@ -226,6 +326,36 @@ func (client *IedClient) digIntoStructure(mms *C.MmsValue, vType MMSType) []GoMm
 		goValue.Value = client.resolveValue(value, MMSType(valueType))
 		goValue.Type = (MMSType)(valueType)
 		goValues = append(goValues, goValue)
+		index++
+	}
+}
+
+// digIntoStructureOptimized 优化版本，减少内存分配和函数调用开销
+func (client *IedClient) digIntoStructureOptimized(mms *C.MmsValue, vType MMSType) []GoMmsValue {
+	if vType != MMS_STRUCTURE {
+		return nil
+	}
+
+	// 预分配一个合理大小的切片（大多数结构体不会超过32个元素）
+	// 这样可以减少大部分情况下的内存重分配
+	goValues := make([]GoMmsValue, 0, 32)
+
+	index := 0
+	for {
+		cIndex := C.int(index)
+		value := C.MmsValue_getElement(mms, cIndex)
+		if value == nil {
+			return goValues
+		}
+
+		valueType := MMSType(C.MmsValue_getType(value))
+
+		// 直接在切片中追加，避免创建临时变量
+		goValues = append(goValues, GoMmsValue{Type: valueType})
+
+		// 使用内联版本减少函数调用开销
+		client.resolveValueInline(&goValues[index], value, valueType)
+
 		index++
 	}
 }
@@ -248,16 +378,28 @@ func (client *IedClient) ReadDataSetValues(dataSetReference string, identifier s
 	values := C.ClientDataSet_getValues(clientDataSet)
 	size := int(C.ClientDataSet_getDataSetSize(clientDataSet))
 
+	// 预分配切片，减少内存分配
 	goValues := make([]GoMmsValue, size)
 
-	// 遍历数据集中的值
-	for i := 0; i < size; i++ {
-		value := C.MmsValue_getElement(values, C.int(i))
-		valueType := MMSType(C.MmsValue_getType(value))
-		// Refer to https://support.mz-automation.de/doc/libiec61850/c/latest/group__MMS__VALUE.html
-		goValues[i].Type = valueType
+	// 使用unsafe优化：获取切片底层数组的指针，减少边界检查
+	if size > 0 {
+		// 直接访问切片底层数组
+		valuesPtr := unsafe.Pointer(&goValues[0])
 
-		goValues[i].Value = client.resolveValue(value, valueType)
+		// 遍历数据集中的值 - 优化：减少C类型转换次数和边界检查
+		for i := 0; i < size; i++ {
+			// 使用指针算术直接访问数组元素，避免切片索引的边界检查
+			target := (*GoMmsValue)(unsafe.Pointer(uintptr(valuesPtr) + uintptr(i)*unsafe.Sizeof(GoMmsValue{})))
+
+			// 减少类型转换：提前转换索引
+			cIndex := C.int(i)
+			value := C.MmsValue_getElement(values, cIndex)
+			valueType := MMSType(C.MmsValue_getType(value))
+
+			target.Type = valueType
+			// 直接内联解析，避免函数调用开销
+			client.resolveValueInline(target, value, valueType)
+		}
 	}
 
 	return goValues, nil
